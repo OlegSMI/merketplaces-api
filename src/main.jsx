b8014d86e79b1ad42a3fba9338f47baeb83fb7c9
Tomykuk <hetagpuhaev@mail.ru>
2024-08-06 12:04:07 +0300

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
